e19b4d175d521afdf2d06feed8dc392fc5d48db0
Josué Santos Pinto <js.pinto55@gmail.com>
2022-05-16 14:02:28 -0300

    finalizando projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,7 @@ let videoarea = document.querySelector('.videoarea')
 let video = document.querySelector('.video')
 let img = document.querySelector('.img')
 let close = document.querySelector('.close')
-let moreBtn = document.querySelector('#morePlans')
-let lessBtn = document.querySelector('#lessPlans')
-let moreClass = document.querySelector('.more')
-let moreArea = document.querySelector('.boxmore')
-let hover = document.querySelector('.more.photosection-plan--photoinfo')
-let photoinfo = document.querySelectorAll('.more .section-plan--photo')
+
 
 
 
@@ -52,36 +47,3 @@ function closeVideo(){
     
  }
 
- moreBtn.addEventListener("click",()=>{
-    
-    let aumMore = setTimeout(openMore,500)
-
-     moreArea.style.height = `0px`
-     moreClass.style.display = 'flex'
-     moreBtn.style.display = 'none'
-     lessBtn.style.display = 'inline-block'
-     
-     function openMore(){
-        moreClass.style.height = '665px'
-     }
-
-
- })
-
- lessBtn.addEventListener("click",()=>{
-    lessBtn.style.display = 'none'
-    
-    let dimMore = setTimeout(closeMore,500)
-
-    moreBtn.style.display = 'inline-block'
-    
-    
-   
-    
-    function closeMore(){
-        moreClass.style.height = '0px'
-        hover.style.display = 'none'
-     }
-})
-
- 
