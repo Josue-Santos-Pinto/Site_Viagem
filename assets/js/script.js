75e9a8072800c7006b1a51b9bcbfbdc7b9e963ce
Josué Santos Pinto <js.pinto55@gmail.com>
2022-05-25 11:27:48 -0300

    add o validador de formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,96 @@ let videoarea = document.querySelector('.videoarea')
 let video = document.querySelector('.video')
 let img = document.querySelector('.img')
 let close = document.querySelector('.close')
+let form = document.querySelector('.validator')
+
+let validator ={
+    handleSubmit:(event)=>{
+        event.preventDefault()
+
+        let send = true
+
+        let inputs = form.querySelectorAll('input')
+
+        validator.clearErrors()
+
+        for(let i = 0; i < inputs.length; i++){
+            let input = inputs[i]
+           let check = validator.checkInput(input)
+
+            if(check !== true){
+                send = false
+                validator.showError(input, check)
+            }
+
+        }
+
+        if(send){
+            form.submit()
+        }
+
+    },
+    checkInput:(input)=>{
+        let rules = input.getAttribute('data-rules')
+        if(rules !== null){
+            rules = rules.split('|')
+            for(let k in rules){
+                let rDetails = rules[k].split('=')
+
+                switch(rDetails[0]){
+                    case 'required':
+
+                        if(input.value == ''){
+                            return 'Campo não pode ser Vazio'
+                        }
+
+                    break
+
+                    case 'min':
+
+                    if(input.value.length < rDetails[1]){
+                        return 'Campo deve ter pelom menos '+ rDetails[1] + ' caracteres'
+                    }
+                    break
+
+                    case 'email':
+
+                    if(input.value != ''){
+                        let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    
+                    if(!regex.test(input.value.toLowerCase())){
+                        return 'Campo de e-mail invalido'
+                    }
+                }
+                    break
+                
+            }
+            }
+        }
+        return true
+    },
+    showError:(input, error)=>{
+        input.style.borderColor= 'red'
+
+        let errorElement = document.createElement('div')
+        errorElement.classList.add('error')
+        errorElement.innerHTML = error
+
+        input.parentElement.insertBefore(errorElement, input.elementSinbling)
+    },
+    clearErrors:()=>{
+        let inputs = form.querySelectorAll('input')
+        for(let i = 0; i < inputs.length; i++){
+            inputs[i].style = ''
+        }
 
 
+        let errorElements = document.querySelectorAll('.error')
+
+        for(let i = 0; i < errorElements.length; i++){
+            errorElements[i].remove()
+        }
+    }
+}
 
 
 document.querySelector('.sliders').style.width = `calc(100vw * ${totalSlides})`
@@ -47,3 +135,4 @@ function closeVideo(){
     
  }
 
+ form.addEventListener('submit',validator.handleSubmit)
